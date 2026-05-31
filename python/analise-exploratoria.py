65b2709dfa16abbe93dcbf14dc21954c07033cd9
Larissa Gomes <larissagomesgaspar@gmail.com>
2026-05-31 18:03:55 -0300

    Criação do elt, onde os dados estão no S3 em CSV e são inseridos atráves de SQL no RDS

--- python/analise-exploratoria.py
+++ python/analise-exploratoria.py
@@ -4,7 +4,7 @@ dados = pd.read_csv('Churn Modeling.csv')
 
 # Informações das cinco primeiras linhas
 print("====== Informações Iniciais ======")
-print(dados.head())
+print(dados.head(10))
 
 # Informações sobre os tipos de cada coluna
 print("\n====== Informações Gerais ======")
@@ -13,3 +13,7 @@ print(dados.info())
 # Informações estatísticas
 print("\n====== Estatística ======")
 print(dados.describe())
+
+# Informações sobre sobrenomes incorretos
+sobrenomes = dados[dados['Surname'].str.contains('\?', regex=True)]
+print(f"\n{sobrenomes}")
